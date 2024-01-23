27bbfa5b4313bee4f992c2556cf385c37662b13c
huy.le3 <huy.le3@mservice.com.vn>
2024-01-23 11:13:11 +0700

    execute js

--- IosNode/ViewController.swift
+++ IosNode/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import JavaScriptCore
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let jsContext = JSContext()
+        
+        jsContext?.evaluateScript(#"""
+        function bitwise(number) {
+            return number << 1;
+        }
+        """#)
+
+        label1.text = "5 << 1 = " + String((jsContext?.evaluateScript("bitwise(5)")?
+            .toInt32())!)
+
     }
 
 
